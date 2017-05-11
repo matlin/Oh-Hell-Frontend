65cbf1a3379452d1b2e18b40ba871af0143d67a0
Philip Bohlman <pbohlman@middlebury.edu>
2017-05-11 18:02:45 -0400

    Added some basic bootstrap styling to the lobby. Sorry Matt

--- src/components/gameLobby.js
+++ src/components/gameLobby.js
@@ -5,6 +5,7 @@
 import React, {Component} from 'react';
 import Server from '../server.js';
 import LobbyModal from './LobbyModal.js';
+import {Panel, ListGroup, ListGroupItem, Button} from 'react-bootstrap';
 import {
   BrowserRouter as Router,
   Route,
@@ -30,12 +31,12 @@ class Lobby extends Component {
      return games.map( game => {
        console.log(game);
        return (
-         <div>
+         <ListGroupItem>
            <Link to={'/game/' + game.id}>
              <span>{game.gameName+' '}</span>
              <span>{game.playersInGame}/{game.maxPlayers}</span>
            </Link>
-         </div>
+         </ListGroupItem>
        );
      });
    }
@@ -44,15 +45,17 @@ class Lobby extends Component {
     let joinedGames = this.gameList(this.state.joinedGames);
     let openGames = this.gameList(this.state.openGames);
     return(
-      <div style={{marginLeft: 3 + 'em'}} id="Lobby">
+      <div style={{marginLeft: 3 + 'em', marginRight: 3 + 'em'}} id="Lobby">
+        <Panel>
         <LobbyModal showModal={this.state.showModal} close={() => this.setState({ showModal: false })}/>
         <h1>Gamelobby</h1>
-        <button type="button" onClick={() => this.setState({ showModal: true })}>Create Game</button>
-        <button type="button" onClick={() => {Server.Game.getGames().then((games)=>{this.setState({joinedGames: games.joinedGames, openGames: games.openGames})})}}>Refresh</button>
+        <Button bsStyle="success" type="button" onClick={() => this.setState({ showModal: true })}>Create Game</Button>
+        <Button bsStyle="primary" type="button" onClick={() => {Server.Game.getGames().then((games)=>{this.setState({joinedGames: games.joinedGames, openGames: games.openGames})})}}>Refresh</Button>
         <h2>Your Games</h2>
-        <div>{joinedGames}</div>
+        <ListGroup>{joinedGames}</ListGroup>
         <h2>Open Games</h2>
-        <div>{openGames}</div>
+        <ListGroup>{openGames}</ListGroup>
+        </Panel>
       </div>
     )
   }
